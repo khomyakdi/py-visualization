b6a632a57e2dc242d2979857c5ac54f6b512cea7
khomyakdi <khomyakdi@gmail.com>
2022-12-10 22:54:22 +0200

    Add numerous walkings

--- rw_visual.py
+++ rw_visual.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk(100)
+while True:
+    rw = RandomWalk()
 
-rw.fill_walk()
+    rw.fill_walk()
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Generate new walking? (y/n): ")
+    if keep_running == 'n':
+        break
