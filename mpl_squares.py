d0dec4c71d0715ced990ca7543a03cc170fdba24
khomyakdi <khomyakdi@gmail.com>
2022-12-10 20:39:38 +0200

    make plot with input values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5, 6, 7]
 squares = [1, 4, 9, 16, 25, 36, 49]
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
