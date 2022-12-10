5df11ee5f52cb65670db56c599dc29fa526d98f8
khomyakdi <khomyakdi@gmail.com>
2022-12-10 20:35:47 +0200

    add formatting

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25, 36, 49]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
