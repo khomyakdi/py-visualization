a65dd51754f46f06678ac05a51d98c51b3693125
khomyakdi <khomyakdi@gmail.com>
2022-12-10 20:41:11 +0200

    Use seaborn styles

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5, 6, 7]
 squares = [1, 4, 9, 16, 25, 36, 49]
+plt.style.use('seaborn')
+
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
