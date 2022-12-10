cfd64672e6c342f67b1d53610e094143bbd3b8f6
khomyakdi <khomyakdi@gmail.com>
2022-12-10 23:00:38 +0200

    Show start/end points

--- rw_visual.py
+++ rw_visual.py
@@ -12,6 +12,9 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
                cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],
+               c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input("Generate new walking? (y/n): ")
