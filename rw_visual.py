ab723a3ae103cd559629df5080f0d207d798c3b4
khomyakdi <khomyakdi@gmail.com>
2022-12-10 23:07:41 +0200

    Use plot to provide path

--- rw_visual.py
+++ rw_visual.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk(50_000)
+    rw = RandomWalk()
 
     rw.fill_walk()
 
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
-               cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1],
                c='red', edgecolors='none', s=100)
