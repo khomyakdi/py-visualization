d530cd5df6c59cd7e4cc6a4adea58fd51e11f875
khomyakdi <khomyakdi@gmail.com>
2022-12-10 21:23:14 +0200

    Show squares in range

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
 
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values,  cmap=plt.cm.Blues, s=10)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
@@ -14,4 +16,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 ax.tick_params(axis="both", which="major", labelsize=14)
 
+ax.axis([0, 1100, 0, 1100000])
+
 plt.show()
+
+plt.savefig('squares_plot.png', bbox_inches="tight")
