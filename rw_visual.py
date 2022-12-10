e99ebaa212c068a455323c717a59880c6dcca2bc
khomyakdi <khomyakdi@gmail.com>
2022-12-10 23:02:14 +0200

    Hide axis

--- rw_visual.py
+++ rw_visual.py
@@ -15,6 +15,9 @@ while True:
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1],
                c='red', edgecolors='none', s=100)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input("Generate new walking? (y/n): ")
