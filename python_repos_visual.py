9a24cc67062eac8e6d625f44f2044f5df0ed8598
khomyakdi <khomyakdi@gmail.com>
2022-12-11 22:20:03 +0200

    Show top starred repositories in plot

---
+++ python_repos_visual.py
@@ -0,0 +1,32 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+
+r = requests.get(url, headers=headers)
+
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+}]
+
+my_layout = {
+    'title': 'Most Starred Python Projects on Github',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stars'}
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
