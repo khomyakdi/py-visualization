2d72cf794340b2d475fc2ee4ec668e2c9a8035a1
khomyakdi <khomyakdi@gmail.com>
2022-12-11 21:42:36 +0200

    Add marker colors according to mag

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -3,7 +3,7 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -24,7 +24,11 @@ data = [{
     'lon': lons,
     'lat': lats,
     'marker': {
-        'size': [5*mag for mag in mags]
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     },
 }]
 my_layout = Layout(title='Global Earthquakes')
