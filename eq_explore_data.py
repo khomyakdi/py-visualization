9d942fbe4948ee6ae52e5fb5f8eb9d3e60f28cc4
khomyakdi <khomyakdi@gmail.com>
2022-12-11 21:44:52 +0200

    Add marker titles

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -9,13 +9,15 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
     mags.append(mag)
 
@@ -23,6 +25,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
