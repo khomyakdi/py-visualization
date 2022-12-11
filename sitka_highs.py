8f1fc0c23b3bbaea88ef779af21be58c5caa2616
khomyakdi <khomyakdi@gmail.com>
2022-12-11 21:11:24 +0200

    Add lows to plot

--- sitka_highs.py
+++ sitka_highs.py
@@ -3,25 +3,28 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     highs = []
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
 
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
-plt.title("Daily high temperature, July 2018", fontsize=24)
+plt.title("Daily high and low temperature - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
