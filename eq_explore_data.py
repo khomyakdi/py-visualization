d9b436635816206843984539633d1f2aed3599f3
khomyakdi <khomyakdi@gmail.com>
2022-12-11 21:38:21 +0200

    Add earthquakes to map plot

---
+++ eq_explore_data.py
@@ -0,0 +1,26 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    lons.append(lon)
+    lats.append(lat)
+
+    mags.append(mag)
+
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
