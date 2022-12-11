ca11e796c67c686711d23d23b2623a6969194063
khomyakdi <khomyakdi@gmail.com>
2022-12-11 21:08:34 +0200

    Add dates to plot

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -7,16 +9,21 @@ with open(filename) as f:
     header_row = next(reader)
 
     highs = []
+    dates, highs = [], []
+
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 plt.style.use('seaborn')
-fix, ax = plt.subplots()
-ax.plot(highs, c='red')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
 
 plt.title("Daily high temperature, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis="both", which='major', labelsize=16)
 
